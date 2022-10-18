310d7a0e2b2668788e8a7cfd9685fb6df4c2ff7f
veesax <rnldvee1@gmail.com>
2022-10-18 17:47:13 +0000

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,12 +4,12 @@
  * main - Entry Point
  *
  * Return : Always 0 (Success)
- */
+*/
 
 int main(void)
 {
 
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+        puts("\"Programming is like building a multilingual puzzle");
+	return(0);
 
 }
